4416d73cf926c3fad7875647d9dcef537d753313
Readper Tsai <readper@17.media>
2018-07-08 00:56:40 +0800

    Add timeout for connection, and handle multiple line

--- main.go
+++ main.go
@@ -3,12 +3,18 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"net"
+	"strings"
+	"time"
 )
 
 const (
 	connType = "tcp"
+	timeout  = 10 * time.Second
+
+	quitCmd = "quit"
 )
 
 var (
@@ -27,10 +33,16 @@ func main() {
 	}
 	defer listener.Close()
 
+	log.Printf("start listening %s", addr.String())
+
 	for {
 		tcpConn, err := listener.AcceptTCP()
 		if err != nil {
 			log.Panicf("acceptTCP failed, error: %s", err.Error())
+		}
+		err = tcpConn.SetDeadline(time.Now().Add(timeout))
+		if err != nil {
+			log.Panicf("set connection deadline failed, error: %s", err.Error())
 		}
 		go handleConnection(tcpConn)
 	}
@@ -40,13 +52,45 @@ func handleConnection(conn *net.TCPConn) {
 	remoteAddr := conn.RemoteAddr()
 	defer log.Printf("connection close from %s", remoteAddr.String())
 	log.Printf("new connect from %s", remoteAddr.String())
-	buffer := make([]byte, 1024)
-	length, err := conn.Read(buffer)
-	if err != nil {
-		log.Printf("read fail, error: %s", err.Error())
-		conn.Close()
-	}
+	for {
+		buffer := make([]byte, 1024)
+		length, err := conn.Read(buffer)
+		if err != nil {
+			if err == io.EOF {
+				log.Println("Read encounter EOF, normal end")
+			} else {
+				log.Printf("read fail, error: %s", err.Error())
+			}
+			conn.Close()
+			return
+		}
+		msg := string(buffer)
+		log.Printf("Got Message: %s size: %d from %s ", msg, length, remoteAddr.String())
 
-	log.Printf("Got Message: %s size: %d from %s ", string(buffer), length, remoteAddr.String())
+		// reset timeout
+		err = conn.SetDeadline(time.Now().Add(timeout))
+		if err != nil {
+			log.Panicf("reset connection deadline failed, error: %s", err.Error())
+		}
+
+		msg = strings.Replace(msg, "\r\n", "\n", -1)
+		msg = strings.Replace(msg, "\r", "\n", -1)
+
+		inputs := strings.Split(msg, "\n")
+		for _, input := range inputs {
+			if input == "" {
+				continue
+			}
+			if input == quitCmd {
+				conn.Close()
+				return
+			}
+			handleInput(input)
+		}
+	}
+}
+
+// each line will be a input
+func handleInput(input string) {
 
 }
